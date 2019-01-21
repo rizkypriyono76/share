9d00a5d499d452b6111fc4111e68a69a8ed5b780
Rizky Priyono <rizkypriyono76@gmail.com>
2019-01-21 23:04:13 +0700

    Membuat post model dengan migration

---
+++ resources/views/blog/comments.blade.php
@@ -0,0 +1,76 @@
+<article class="post-comments">
+    <h3 class="comment-heading">5 Comments</h3>
+    <div class="comment-body padding-10">
+        <ul class="comments-list">
+            <li class="comment-item">
+                <h4>John Doe <small>January 14, 2016</small></h4>
+                <div class="comment-content">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio nesciunt nulla est, atque ratione nostrum cumque ducimus maxime, amet enim tempore ipsam. Id ea, veniam ipsam perspiciatis assumenda magnam doloribus!</p>
+                    <p>Quibusdam iusto culpa, necessitatibus, libero sequi quae commodi ea ab non facilis enim vitae inventore laborum hic unde esse debitis. Adipisci nostrum reprehenderit explicabo, non molestias aliquid quibusdam tempore. Vel.</p>
+                </div>
+            </li>
+
+            <li class="comment-item">
+                <h4>Jane Doe <small>January 13, 2016</small></h4>
+                <div class="comment-content">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio nesciunt nulla est, atque ratione nostrum cumque ducimus maxime, amet enim tempore ipsam. Id ea, veniam ipsam perspiciatis assumenda magnam doloribus!</p>
+                    <p>Quibusdam iusto culpa, necessitatibus, libero sequi quae commodi ea ab non facilis enim vitae inventore laborum hic unde esse debitis. Adipisci nostrum reprehenderit explicabo, non molestias aliquid quibusdam tempore. Vel.</p>
+
+                    <ul class="comments-list-children">
+                        <li class="comment-item">
+                            <h4>John Doe <small>January 14, 2016</small></h4>
+                            <div class="comment-content">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio nesciunt nulla est, atque ratione nostrum cumque ducimus maxime, amet enim tempore ipsam. Id ea, veniam ipsam perspiciatis assumenda magnam doloribus!</p>
+                                <p>Quibusdam iusto culpa, necessitatibus, libero sequi quae commodi ea ab non facilis enim vitae inventore laborum hic unde esse debitis. Adipisci nostrum reprehenderit explicabo, non molestias aliquid quibusdam tempore. Vel.</p>
+                            </div>
+                        </li>
+
+                        <li class="comment-item">
+                            <h4>Jane Doe <small>January 13, 2016</small></h4>
+                            <div class="comment-content">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio nesciunt nulla est, atque ratione nostrum cumque ducimus maxime, amet enim tempore ipsam. Id ea, veniam ipsam perspiciatis assumenda magnam doloribus!</p>
+                                <p>Quibusdam iusto culpa, necessitatibus, libero sequi quae commodi ea ab non facilis enim vitae inventore laborum hic unde esse debitis. Adipisci nostrum reprehenderit explicabo, non molestias aliquid quibusdam tempore. Vel.</p>
+
+                                <ul class="comments-list-children">
+                                    <li class="comment-item">
+                                        <h4>John Doe <small>January 14, 2016</small></h4>
+                                        <div class="comment-content">
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio nesciunt nulla est, atque ratione nostrum cumque ducimus maxime, amet enim tempore ipsam. Id ea, veniam ipsam perspiciatis assumenda magnam doloribus!</p>
+                                            <p>Quibusdam iusto culpa, necessitatibus, libero sequi quae commodi ea ab non facilis enim vitae inventore laborum hic unde esse debitis. Adipisci nostrum reprehenderit explicabo, non molestias aliquid quibusdam tempore. Vel.</p>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+
+    </div>
+
+    <div class="comment-footer padding-10">
+        <h3 class="comment-heading">Leave a comment</h3>
+        <p>Required fields are marked <span class="required">*</span></p>
+        <form>
+            <div class="form-group">
+                <label for="name">Name *</label>
+                <input type="text" name="name" id="name" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="email">Email *</label>
+                <input type="text" name="email" id="email" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="website">Website</label>
+                <input type="text" name="website" id="website" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="comment">Comment</label>
+                <textarea name="comment" id="comment" rows="6" class="form-control"></textarea>
+            </div>
+            <button type="submit" class="btn btn-lg btn-success">Submit</button>
+        </form>
+    </div>
+
+</article>
--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,98 +1,103 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends ('layouts.main')
 
-        <title>Laravel</title>
+@section ('content')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+<div class="container">
+    <div class="row">
+        <div class="col-md-8">
+            <article class="post-item">
+                <div class="post-item-image">
+                    <a href="post.html">
+                        <img src="{{asset('img/post1.jpg')}}" alt="">
+                    </a>
                 </div>
-            @endif
+                <div class="post-item-body">
+                    <div class="padding-10">
+                        <h2><a href="post.html">Lorem ipsum dolor sit amet, consectetur adipisicing elit</a></h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos voluptas, blanditiis impedit repellat harum, eaque saepe aspernatur quo magnam obcaecati dolor! Deleniti quod repellendus non iste architecto, voluptate excepturi velit.</p>
+                    </div>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+                    <div class="post-meta padding-10 clearfix">
+                        <div class="pull-left">
+                            <ul class="post-meta-group">
+                                <li><i class="fa fa-user"></i><a href="#"> Admin</a></li>
+                                <li><i class="fa fa-clock-o"></i><time> February 12, 2016</time></li>
+                                <li><i class="fa fa-tags"></i><a href="#"> Blog</a></li>
+                                <li><i class="fa fa-comments"></i><a href="#">4 Comments</a></li>
+                            </ul>
+                        </div>
+                        <div class="pull-right">
+                            <a href="post.html">Continue Reading &raquo;</a>
+                        </div>
+                    </div>
                 </div>
+            </article>
+            <article class="post-item">
+                <div class="post-item-image">
+                    <a href="post.html">
+                        <img src="/img/Post+Image+2.jpg" alt="">
+                    </a>
+                </div>
+                <div class="post-item-body">
+                    <div class="padding-10">
+                        <h2><a href="post.html">Lorem ipsum dolor sit amet, consectetur adipisicing elit</a></h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos voluptas, blanditiis impedit repellat harum, eaque saepe aspernatur quo magnam obcaecati dolor! Deleniti quod repellendus non iste architecto, voluptate excepturi velit.</p>
+                    </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <div class="post-meta padding-10 clearfix">
+                        <div class="pull-left">
+                            <ul class="post-meta-group">
+                                <li><i class="fa fa-user"></i><a href="#"> Admin</a></li>
+                                <li><i class="fa fa-clock-o"></i><time> February 12, 2016</time></li>
+                                <li><i class="fa fa-tags"></i><a href="#"> Blog</a></li>
+                                <li><i class="fa fa-comments"></i><a href="#">4 Comments</a></li>
+                            </ul>
+                        </div>
+                        <div class="pull-right">
+                            <a href="post.html">Continue Reading &raquo;</a>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            </article>
+            <article class="post-item">
+                <div class="post-item-image">
+                    <a href="post.html">
+                        <img src="/img/Post+Image+3.jpg" alt="">
+                    </a>
+                </div>
+                <div class="post-item-body">
+                    <div class="padding-10">
+                        <h2><a href="post.html">Lorem ipsum dolor sit amet, consectetur adipisicing elit, consectetur adipisicing elit</a></h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos voluptas, blanditiis impedit repellat harum, eaque saepe aspernatur quo magnam obcaecati dolor! Deleniti quod repellendus non iste architecto, voluptate excepturi velit.</p>
+                    </div>
+
+                    <div class="post-meta padding-10 clearfix">
+                        <div class="pull-left">
+                            <ul class="post-meta-group">
+                                <li><i class="fa fa-user"></i><a href="#"> Admin</a></li>
+                                <li><i class="fa fa-clock-o"></i><time> February 12, 2016</time></li>
+                                <li><i class="fa fa-tags"></i><a href="#"> Vue Js</a>, <a href="#"> Laravel</a></li>
+                                <li><i class="fa fa-comments"></i><a href="#">4 Comments</a></li>
+                            </ul>
+                        </div>
+                        <div class="pull-right">
+                            <a href="post.html">Continue Reading &raquo;</a>
+                        </div>
+                    </div>
+                </div>
+            </article>
+
+            <nav>
+                <ul class="pager">
+                    <li class="previous disabled"><a href="#"><span aria-hidden="true">&larr;</span> Newer</a></li>
+                    <li class="next"><a href="#">Older <span aria-hidden="true">&rarr;</span></a></li>
+                </ul>
+            </nav>
         </div>
-    </body>
-</html>
+
+    @include('layouts.sidebar')
+
+    </div>
+</div>
+
+@endsection
